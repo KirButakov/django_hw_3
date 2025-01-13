f79c94a8881a138b250be0067c235bce4074cebf
Kira <kirillbutakov02021994@gmail.com>
2025-01-13 08:08:53 +0300

    hw

--- my_project/courses/serializers.py
+++ my_project/courses/serializers.py
@@ -1,29 +1,19 @@
-from rest_framework import serializers
-from .models import Course, Lesson, Subscription
-from .validators import validate_video_url
-
-class LessonSerializer(serializers.ModelSerializer):
-    video_url = serializers.URLField(validators=[validate_video_url])  # Валидатор применяется только к полю video_url
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
-
-
 class CourseWithLessonsSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)  # Поле lessons только для чтения
+    is_subscribed = serializers.SerializerMethodField()  # Добавляем поле для подписки
 
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'lesson_count', 'lessons']
+        fields = ['id', 'name', 'description', 'lesson_count', 'lessons', 'is_subscribed']  # Добавлено поле is_subscribed
 
     def get_lesson_count(self, obj):
         # Возвращаем количество уроков для курса
         return obj.lessons.count()
 
-
-class SubscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ['user', 'course']
+    def get_is_subscribed(self, obj):
+        # Проверяем, подписан ли текущий пользователь на курс
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return Subscription.objects.filter(user=user, course=obj).exists()
+        return False  # Если пользователь не аутентифицирован, возвращаем False
