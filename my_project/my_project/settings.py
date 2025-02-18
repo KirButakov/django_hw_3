a80f7cbfa4bc2ecc4c4a7486034121d5b09f9f59
Kira <kirillbutakov02021994@gmail.com>
2025-02-18 11:10:00 +0300

    hw

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
